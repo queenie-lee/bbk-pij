243493ec4af9d588d7ebb2834e3b1e3a3e8d3aee
queenie-lee <queenie.lee@live.ca>
2023-10-11 11:31:40 +0100

    Removed unnecessary boolean variable. Amended code to work when one or both numbers are negative.

--- Exercises_d1_5.java
+++ Exercises_d1_5.java
@@ -1,3 +1,8 @@
+/*
+Write a program that requests two numbers from the user and then outputs their product. You can-
+not use the \*" operator.
+Can you make your program work correctly also if one or both numbers are negative?
+*/
 public class Exercises_d1_5 {
     public static void main(String[] args) {
         java.util.Scanner scan = new java.util.Scanner(System.in);
@@ -7,11 +12,24 @@ public class Exercises_d1_5 {
         int num2 = scan.nextInt();
         int counter = 0;
         int product = 0;
-        boolean prime = true;
+        int sign = 1;
+        
+        if (num1 < 0){
+            num1 = num1 * (-1);
+            sign = sign * (-1);
+            if (num2 < 0){
+                num2 = num2 * (-1);
+                sign = sign * (-1);
+            }
+        } else if (num2 < 0){
+            num2 = num2 * (-1);
+            sign = sign * (-1);
+        }
         while (counter < num2) {
             product = product + num1;
             counter = counter + 1;
         }
+        product = product * sign;
         System.out.println("The product is: " + product);
     }
 }

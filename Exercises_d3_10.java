c6fee0e35953a03fba78ad20ee606a8d37e25d48
queenie-lee <queenie.lee@live.ca>
2023-10-16 16:34:18 +0100

    Modified to read text and letter from the user. Program outputs the number of times the user's letter is found in the text, rather than a fixed letter set by program

--- Exercises_d3_10.java
+++ Exercises_d3_10.java
@@ -8,20 +8,24 @@ gram should then say how many times you can  nd the letter in the text.
 public class Exercises_d3_10 {
     public static void main(String[] args) {
         java.util.Scanner scan = new java.util.Scanner(System.in);
-        char letter = 'e';
+        //char letter = 'e';
         int letterCounter = 0;
         int counter = 0;
 
         System.out.print("Enter some text: ");
         String text = scan.nextLine();
 
+        System.out.print("Enter the letter you wish to count: ");
+        String letter = scan.nextLine();
+        char l = letter.charAt(0);
+
         while (counter < text.length()) {
-            if (text.charAt(counter) == letter) {
+            if (text.charAt(counter) == l) {
                 letterCounter ++;
             }
             counter ++;
         }
 
-        System.out.print("There are " + letterCounter + " \'" + letter + "\'s in your text");
+        System.out.print("There are " + letterCounter + " \'" + l + "\'s in your text");
     }
 }
